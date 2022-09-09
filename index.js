1a9f2a076b01271f2dcb370b471454c5fd9b6b4d
Akramsarker <dev.mdakram@gmail.com>
2022-09-09 23:00:28 +0600

    Learning some methods on express js

--- index.js
+++ index.js
@@ -2,17 +2,43 @@ const experss = require("express");
 
 const app = experss();
 
-app.get("/", (req, res) => {
+// app.use(experss.json());
+// app.use(experss.raw());
+// app.use(experss.text());
+// app.use(experss.urlencoded());
+
+// app.use(
+//   express.static(`${__dirname}/public/`, {
+//     index: "home.html",
+//   })
+// );
+
+var router = express.Router();
+
+app.use(router);
+
+// app.get("/", (req, res) => {
+//   // console.log(req.body.name);
+
+//   res.send("This is home page");
+// });
+
+router.get("/about", (req, res) => {
   res.send("This is home page");
 });
 
 app.post("/", (req, res) => {
-  res.send({
-    name: "Md akram sheikh",
-    email: "dev.mdakram@gmail.com",
-    phoneNumber: "01790825863",
-    city: "Bogura",
-  });
+  // console.log(req.body.toString());
+  // console.log(req.body);
+
+  // res.send({
+  //   // name: "Md akram sheikh",
+  //   // email: "dev.mdakram@gmail.com",
+  //   // phoneNumber: "01790825863",
+  //   // city: "Bogura",
+  //   // name: "This is post request",
+  // });
+  res.send("This is home page with post request");
 });
 
 app.listen(3000, () => {
